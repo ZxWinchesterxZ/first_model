57a7d66165015f7ab58b42e5adb7ee14aec68e50
ZxWinchesterxZ <ahmed.emad.a.e.m@gmail.com>
2022-04-25 17:37:10 +0200

    dd

--- m.py
+++ m.py
@@ -2,10 +2,14 @@ import tensorflow as tf
 from nltk.stem.porter import PorterStemmer
 from fastapi import FastAPI
 import numpy as np
-import nltk
-stemmer = PorterStemmer()
+from nltk import word_tokenize
+all_words = []
+label=[]
+f1 = open("all_words.txt", "r")
+all_words = f1.read().splitlines()
+f1.close()
 def bag_of_words(tokenized_sentence, all_words):
-    tokenized_sentence = [stemmer.stem(w.lower()) for w in tokenized_sentence]
+    tokenized_sentence = [PorterStemmer().stem(w.lower()) for w in tokenized_sentence]
 
     bag = np.zeros(len(all_words), dtype=np.float32)
     for idx, w in enumerate(all_words):
@@ -13,12 +17,7 @@ def bag_of_words(tokenized_sentence, all_words):
             bag[idx] = 1.0
 
     return bag
-all_words = []
-f1 = open("all_words.txt", "r")
-all_words = f1.read().splitlines()
-f1.close()
 m = tf.keras.models.load_model('diseases.h5')
-label=[]
 f1 = open("labels.txt", "r")
 label = f1.read().splitlines()
 f1.close()
@@ -30,10 +29,12 @@ def index():
 
 @app.get('/predict_disease')
 def predict_disease(phrase:str):
-    sentence = nltk.word_tokenize(phrase)
+    sentence = word_tokenize(phrase)
     X = bag_of_words(sentence, all_words)
     X = X.reshape(1, X.shape[0])
     output = m.predict(X)
     return {
         'prediction': label[np.argmax(output)]
     }
+
+
