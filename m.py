55f65eedff8111e4d132fde7e5a4fcdc0ab72b2f
ZxWinchesterxZ <ahmed.emad.a.e.m@gmail.com>
2022-04-26 09:54:53 +0200

    last

--- m.py
+++ m.py
@@ -1,7 +1,7 @@
 import keras
 from nltk.stem.porter import PorterStemmer
 from fastapi import FastAPI
-from numpy import argmax
+from numpy import argmax , zeros,float32
 from nltk import word_tokenize
 all_words = []
 label=[]
@@ -11,7 +11,7 @@ f1.close()
 def bag_of_words(tokenized_sentence, all_words):
     tokenized_sentence = [PorterStemmer().stem(w.lower()) for w in tokenized_sentence]
 
-    bag = np.zeros(len(all_words), dtype=np.float32)
+    bag = zeros(len(all_words), dtype=float32)
     for idx, w in enumerate(all_words):
         if w in tokenized_sentence:
             bag[idx] = 1.0
